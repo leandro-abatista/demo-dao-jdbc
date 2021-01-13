efa96192b6ae74d64e5a806b8fa43dafbe4dde38
Leandro <Leandro@DESKTOP-SHKFPNB>
2021-01-13 14:06:13 -0300

    Testando a classe modelo vendedor.

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Departamento obj = new Departamento(1L, "Books");
-		System.out.println(obj);
+		Vendedor vendedor = new Vendedor(21L, "Bob", "8399999999", "teste@teste.com.br", new Date(), 3000.0, obj);
+		System.out.println(vendedor);
 
 	}
 
