f21be428f025a7f7658b7ffd74341edecd4b3133
Leandro <Leandro@DESKTOP-SHKFPNB>
2021-01-13 16:20:35 -0300

    Foi criado a classe de implementação VendedorDaoImplJdbc e o Daofactory.

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.VendedorDao;
 import model.entities.Departamento;
 import model.entities.Vendedor;
 
@@ -13,6 +15,8 @@ public class Program {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Departamento obj = new Departamento(1L, "Books");
 		Vendedor vendedor = new Vendedor(21L, "Bob", "8399999999", "teste@teste.com.br", new Date(), 3000.0, obj);
+		
+		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
 		System.out.println(vendedor);
 
 	}
