95c7671d34ab03fc68075cf50bdc40574cac016f
Leandro <Leandro@DESKTOP-SHKFPNB>
2021-01-13 16:11:30 -0300

    Criado a interface de departamento e vendedor.

---
+++ demo-dao-jdbc/src/model/dao/DepartamentoDao.java
@@ -0,0 +1,19 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Departamento;
+
+public interface DepartamentoDao {
+	
+	public void insert(Departamento obj);
+	
+	public void update(Departamento obj);
+	
+	public void deleteById(Long id);
+	
+	public Departamento findById(Long id);
+	
+	List<Departamento> findAll();
+
+}
---
+++ demo-dao-jdbc/src/model/dao/VendedorDao.java
@@ -0,0 +1,19 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Vendedor;
+
+public interface VendedorDao {
+	
+public void insert(Vendedor obj);
+	
+	public void update(Vendedor obj);
+	
+	public void deleteById(Long id);
+	
+	public Vendedor findById(Long id);
+	
+	List<Vendedor> findAll();
+
+}
